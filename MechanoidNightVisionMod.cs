a9d89d7efb9b6d42d1581499a47e3d72b34d4cfd
Benjamin E. Iserman <winggar1228@gmail.com>
2022-11-05 18:35:26 -0500

    Add project files.

---
+++ MechanoidNightVisionMod.cs
@@ -0,0 +1,30 @@
+﻿namespace MechanoidNightVision
+{
+	using System.IO;
+	using System.Reflection;
+	using HarmonyLib;
+	using RimWorld;
+	using Verse;
+
+	internal class MechanoidNightVision : Mod
+	{
+		private static readonly string Identifier = "MechanoidNightVision";
+		internal static string VersionDir => Path.Combine(ModLister.GetActiveModWithIdentifier($"winggar.{Identifier}").RootDir.FullName, "Version.txt");
+		public static string CurrentVersion { get; private set; }
+
+		public MechanoidNightVision(ModContentPack content) : base(content)
+		{
+			var version = Assembly.GetExecutingAssembly().GetName().Version;
+			CurrentVersion = $"{version.Major}.{version.Minor}.{version.Build}";
+
+			if (Prefs.DevMode)
+			{
+				File.WriteAllText(VersionDir, CurrentVersion);
+			}
+
+			new Harmony($"winggar.{Identifier}").PatchAll();
+		}
+
+		public override string SettingsCategory() => Identifier.Translate();
+	}
+}
---
+++ Patch_StatPart_Glow.cs
@@ -0,0 +1,35 @@
+﻿namespace MechanoidNightVision
+{
+	using System.Reflection;
+	using HarmonyLib;
+	using RimWorld;
+	using Verse;
+
+	[HarmonyPatch]
+	internal class Patch_StatPart_Glow
+	{
+		static MethodBase TargetMethod() => typeof(StatPart_Glow).GetMethod("ActiveFor", BindingFlags.NonPublic | BindingFlags.Instance);
+
+		[HarmonyPrefix]
+		static bool Prefix(ref bool __result, bool ___humanlikeOnly, Thing t)
+		{
+			if (t is Pawn pawn)
+			{
+				if (___humanlikeOnly && !pawn.RaceProps.Humanlike)
+				{
+					__result = false;
+					return false;
+				}
+
+				if (pawn.RaceProps.IsMechanoid)
+				{
+					__result = false;
+					return false;
+				}
+			}
+
+			__result = t.Spawned;
+			return false;
+		}
+	}
+}
